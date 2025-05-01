4e87e49a53eabd4450ece0f27c99356d3ad210b5
bienvenudev <cbienvenu007@gmail.com>
2025-05-01 13:31:25 +0200

    feat: disable button while fetching

--- projects/10_Dad_Jokes/script.js
+++ projects/10_Dad_Jokes/script.js
@@ -1,23 +1,37 @@
-const jokeEl = document.querySelector('#joke');
-const jokeBtn = document.querySelector('#jokeBtn');
+const jokeButton = document.querySelector("#jokeBtn");
+const jokeText = document.querySelector("#joke");
 
 generateJoke();
 
-jokeBtn.addEventListener('click', generateJoke);
+jokeButton.addEventListener("click", generateJoke);
 
-// Using async/await
 async function generateJoke() {
-	const config = {
-		headers : {
-		'Accept': 'application/json'
+	jokeButton.disabled = true;
+	jokeButton.textContent = "Loading...";
+
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  try {
+    const response = await fetch("https://icanhazdadjoke.com/", config);
+    const data = await response.json();
+
+    displayJoke(data.joke);
+	} catch (err) {
+		displayJoke("Failed to fetch a joke. Please try again.")
+    console.error("Error:", err);
+	} finally {
+		jokeButton.disabled = false;
+		jokeButton.textContent = "Get Another Joke";
 	}
-};
+}
 
-	const response =	await fetch('https://icanhazdadjoke.com/', config);
-	const data = await response.json();
-
-	jokeEl.innerText = data.joke
-} 
+function displayJoke(joke) {
+  jokeText.textContent = joke;
+}
 
 // Using .then()
 /* 
